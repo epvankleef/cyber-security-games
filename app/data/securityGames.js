7dd51223a39b562a1b4083f3936980ddd8405e6a
epvankleef <epvankleef@gmail.com>
2025-04-08 13:49:05 +0200

    Icon changes

--- app/data/securityGames.js
+++ app/data/securityGames.js
@@ -1,5 +1,5 @@
 import {
-  Newspaper, Heart, Mail, Eye, ShieldQuestion, Video,
+  Newspaper, Heart, Mail, Eye, ShieldQuestion, Video, Telescope,
   Bitcoin, IdCard, MessageSquareMore, AlarmClock, Fish, BookOpenCheck 
 } from 'lucide-react';
 
@@ -11,11 +11,11 @@ export const securityGames = [
   { id: 5, name: 'Grooming', icon: Eye, color: 'bg-purple-500', route: 'grooming', description: 'Simuleer en herken foute intenties' },
   { id: 6, name: 'Phishing Quiz', icon: ShieldQuestion, color: 'bg-pink-400', route: 'phishingquiz', description: 'Zijn de berichten echt? Test je kennis over phishing.' },
   { id: 7, name: 'Crypto Scam', icon: Bitcoin, color: 'bg-blue-500', route: 'cryptoscam', description: 'Leer crypto-oplichting op tijd herkennen' },
-  { id: 8, name: 'Escape Room', icon: IdCard, color: 'bg-teal-400', route: 'datagevaar', description: 'Jouw data in gevaar, of niet?', externalUrl: 'https://hackaton.badeendensoep.nl/Pages/levelSelect.html' },
+  { id: 8, name: 'Escape Room', icon: Telescope, color: 'bg-teal-400', route: 'datagevaar', description: 'Jouw data in gevaar, of niet?', externalUrl: 'https://hackaton.badeendensoep.nl/Pages/levelSelect.html' },
   { id: 9, name: 'Snap Child', icon: MessageSquareMore, color: 'bg-orange-400', route: 'snapchild', description: 'Test je instinct bij online gesprekken' },
   { id: 10, name: 'Click Quiz', icon: AlarmClock, color: 'bg-green-400', route: 'clickquiz', description: 'Test je reflexen en cybersecuritykennis tegelijk.' },
   { id: 11, name: 'Fake News Game', icon: BookOpenCheck , color: 'bg-red-400', route: 'fakenewsgame', description: 'Ontmasker het nepnieuws: feit of fabel?' },
-    { id: 12, name: 'Reality Check', icon: Video , color: 'bg-purple-400', route: 'realitycheck', description: 'Herken jij de deepfake? Speur naar nepbeelden!', externalUrl: 'https://halalmf.itch.io/reality-check?classId=931afd01-b366-4b9a-bfbd-7e8a52fc0b6b&assignmentId=91cd9fc4-d841-4e47-a58d-d72569578122&submissionId=55d4e5cb-311a-005b-2d50-70a6d668c10f' },
+  { id: 12, name: 'Reality Check', icon: Video , color: 'bg-purple-400', route: 'realitycheck', description: 'Herken jij de deepfake? Speur naar nepbeelden!', externalUrl: 'https://halalmf.itch.io/reality-check?classId=931afd01-b366-4b9a-bfbd-7e8a52fc0b6b&assignmentId=91cd9fc4-d841-4e47-a58d-d72569578122&submissionId=55d4e5cb-311a-005b-2d50-70a6d668c10f' },
 
 
 
